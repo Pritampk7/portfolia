39a5fb0906f8a8fc078718534f65a852f889c57c
Sujit <sujit.patil72@hotmail.com>
2021-11-27 11:17:07 +0530

    added experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ interface ISkills {
   primary: { name: string, value: String }[],
   secondary: { name: string, value: String }[],
 }
+interface IExperience {
+  company: string, date: String, position: String, work: String[],
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +14,7 @@ interface ISkills {
 })
 export class AppComponent {
   title = 'portfolio';
+
   skills: ISkills = {
     primary: [
       { name: 'Angular', value: '90' },
@@ -26,5 +30,52 @@ export class AppComponent {
       { name: 'AWS', value: '30' },
       { name: 'Electron.js', value: '60' },
     ]
+  }
+
+  experience: IExperience[] = [
+    {
+      company: "DOXIVA- Doctor's Intelligent Virtual Assistant",
+      date: '09/2020 - Present',
+      position: 'Senior Full Stack Software Engineer (Angular/Node.js)',
+      work: [
+        'Built design in accordance with Atomic Design methodology to boost performance.',
+        'Built high performing application that steadily loads content.',
+        'Added video calling feature with Opentok SDK which provides low latency and high fidelity.',
+        'Improved customer experience by adding Speech to Text capability.',
+        'Implemented end to end dynamic questionnaire for Patient'
+      ],
+    },
+    {
+      company: "Hashtaag™ - KGJ Software Technologies",
+      date: '06/2019 - 09/2020',
+      position: 'Angular Developer',
+      work: [
+        'Responsible for overall web project management from development to execution and maintenance',
+        'Improved clients internal operating efficiency by over 40%.',
+        'Design and implement web architecture of Intranet site.',
+      ],
+    },
+    {
+      company: "Mahiti Infotech",
+      date: '08/2018 - 06/2019',
+      position: 'Angular Developer',
+      work: [
+        'Developed a dynamic and user-friendly website that ensures high traffic and User experience.',
+        'Integrated payment gateways for online money transfer.',
+      ],
+    },
+    {
+      company: "VebCrumbs Innovations",
+      date: '04/2017 - 05/2018',
+      position: 'Full Stack Developer',
+      work: [
+        'Composed ERP software for college management',
+        'Manages different activities for students like creating student profile, adding academics, maintaining contacts.',
+      ],
+    },
+  ]
+
+  open() {
+    window.open('https://en.wikipedia.org/wiki/Web_developer')
   }
 }
