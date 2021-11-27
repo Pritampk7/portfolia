1999bef50fdf0206bc72e542bee36f5836b0e546
Sujit <sujit.patil72@hotmail.com>
2021-11-27 09:07:02 +0530

    addded primary and secondary skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface ISkills {
+  primary: { name: string, value: String }[],
+  secondary: { name: string, value: String }[],
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +11,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  skills: ISkills = {
+    primary: [
+      { name: 'Angular', value: '90' },
+      { name: 'JavaScript', value: '75' },
+      { name: 'Node.js', value: '70' },
+    ],
+    secondary: [
+      { name: 'HTML', value: '85' },
+      { name: 'Express.js', value: '70' },
+      { name: 'CSS', value: '80' },
+      { name: 'MongoDB', value: '60' },
+      { name: 'SASS', value: '75' },
+      { name: 'AWS', value: '30' },
+      { name: 'Electron.js', value: '60' },
+    ]
+  }
 }
-
-//test
